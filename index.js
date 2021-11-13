357fe3bade67b38a3a47eb7d726bc67126a59f9f
Simrri Salazar <ssimrri@gmail.com>
2021-11-12 21:40:50 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let newArr = [];
+    for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+return newArr;
+} 
+
+function countDown(positiveNumber) {
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
